4191c3cc6ff6b31fdf63e197e27c8ae75897e6a0
InkyzProd <63397756+InkyzProd@users.noreply.github.com>
2023-06-26 12:48:44 +0000

    Update index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { AoiClient, LoadCommands } = require("aoi.js");
+
+const Client = new AoiClient({
+    token: "Discord Bot Token",
+    prefix: "Discord Bot Prefix",
+    intents: ["MessageContent", "Guilds", "GuildMessages"],
+    events: ["onMessage", "onInteractionCreate"],
+    database: {
+        type: "aoi.db",
+        db: require("aoi.db"),
+        tables: ["main"],
+        path: "./database/",
+        extraOptions: {
+            dbType: "KeyValue"
+        }
+    }
+});
+
+const cmdload = new LoadCommands(Client);
+cmdload.load(Client.cmd, "./commands/")
